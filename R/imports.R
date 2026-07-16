da7bdd963d1e4877ff9671849d5d57c8fe89ba2c
Kabir Olorede <kabir.olorede@kwasu.edu.ng>
2026-07-16 12:32:04 +0100

    Fix CI and pkgdown configuration

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,7 @@
 #' Package imports
 #'
 #' @name risdr-imports
+#' @keywords internal
 #' @importFrom graphics abline barplot legend matplot par
 #' @importFrom stats predict
 #' @importFrom utils head
